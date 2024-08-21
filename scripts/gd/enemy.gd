99dbe0f3b7c5bc329b1f71962b9fb3bfb4e0ec77
Sonu <bhaarat.mohanta@gmail.com>
2024-08-20 23:56:52 -0500

    feat: some final code stuff

--- scripts/gd/enemy.gd
+++ scripts/gd/enemy.gd
@@ -5,25 +5,27 @@ var player
 var array_pos : int = 0
 var last_position : Vector2
 var state : bool = true
+var active : bool = true
 func _ready():
 	self.global_position.x = 0
 	self.global_position.y = 0
 	$AnimatedSprite2D.play("idle")
 
 func _physics_process(_delta):
-	last_position = self.global_position
-	player = get_parent().get_node("player")
-	if player != null:
-		if player.get_death_status():
-			state = true
-	if player != null and $Spawn_Timer.is_stopped() and state:
-		player_pos = player.get_player_position_array()
-		player_animation = player.get_player_animation_array()
-		if not player.get_death_status():
-			self.global_position = player_pos[array_pos]
-			$AnimatedSprite2D.play(player_animation[array_pos])
-			$AnimatedSprite2D.flip_h = false if self.global_position.x - player.global_position.x < 0 else true
-			array_pos += 1
+	if active:
+		last_position = self.global_position
+		player = get_parent().get_node("player")
+		if player != null:
+			if player.get_death_status():
+				state = true
+		if player != null and $Spawn_Timer.is_stopped() and state:
+			player_pos = player.get_player_position_array()
+			player_animation = player.get_player_animation_array()
+			if not player.get_death_status():
+				self.global_position = player_pos[array_pos]
+				$AnimatedSprite2D.play(player_animation[array_pos])
+				$AnimatedSprite2D.flip_h = false if self.global_position.x - player.global_position.x < 0 else true
+				array_pos += 1
 
 
 func _on_area_2d_body_entered(body):
@@ -39,3 +41,7 @@ func _on_area_2d_body_entered(body):
 
 func _on_spawn_timer_timeout():
 	$Area2D/CollisionShape2D.set_deferred("disabled", false)
+
+func active_switch():
+	$AnimatedSprite2D.play("death")
+	active = false
--- scripts/gd/main_menu.gd
+++ scripts/gd/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/world_2.tscn")
+	get_tree().change_scene_to_file("res://scenes/world_1.tscn")
 
 
 func _on_quit_button_pressed():
