5b9d102035270e8c01c149c3337267dd36bea0a3
Sonu <bhaarat.mohanta@gmail.com>
2024-08-23 23:55:17 -0500

    feat: remade level 1

--- scripts/gd/world/main_menu.gd
+++ scripts/gd/world/main_menu.gd
@@ -4,11 +4,11 @@ func _ready():
 	$VBoxContainer/start_button.grab_focus()
 func _on_start_button_pressed():
 	MainMenuButtonSfx.main_menu_click()
-	get_tree().change_scene_to_file("res://scenes/world_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/world_1.tscn")
 	
 func _on_controls_button_pressed():
 	MainMenuButtonSfx.main_menu_click()
-	get_tree().change_scene_to_file("res://scenes/controls.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/controls.tscn")
 
 func _on_quit_button_pressed():
 	MainMenuButtonSfx.main_menu_click()
